8c0f54114a9480514aa2105cf1c8680f89192fdd
Lucas Perard <perardlucas@gmail.com>
2019-12-29 16:39:38 +0100

    started tcp serialization, not tested yet

--- src/Connexion.java
+++ src/Connexion.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.*;
 
@@ -17,5 +18,29 @@ public class Connexion {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public void TCPserializeReceive () {
+		try {
+			ObjectInputStream objectInput = new ObjectInputStream(this.sock.getInputStream());
+			Object ObjReceived = objectInput.readObject();
+			if (ObjReceived.getClass().toString().compareTo("class MessageSys") == 0) {
+        		MessageSys message_sys_received = (MessageSys) ObjReceived;
+        		System.out.println("TCP: received serialized Message Sys");
+        		System.out.println(message_sys_received.toString());
+        	}
+        	else if (ObjReceived.getClass().toString().compareTo("class Message") == 0){
+        		Message message_received = (Message) ObjReceived;
+        		System.out.println("TCP: received serialized Message");
+        		System.out.println(message_received.toString());
+        	}
+        	else {
+        		System.out.println("On est dans les choux");
+        	}
+			} 
+		catch (IOException|ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	
 	}
 }
